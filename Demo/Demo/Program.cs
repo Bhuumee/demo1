3c7c0dbb0f19cd9e7ce5c88c52a8312c27e8e250
labuser <bhoomika>
2023-02-16 16:21:42 +0000

    initial commit

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,40 @@
+﻿//using System;
+
+
+//namespace Demo
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			int age = int.Parse(Console.ReadLine());
+//			if (age >= 18)
+//				Console.WriteLine("Eligible to vote");
+//			else
+//				Console.WriteLine("Not Eligible");
+
+//			Console.ReadLine();
+//		}
+//	}
+//}
+
+
+
+
+//using System;
+
+
+//namespace Demo
+//{
+//	class Program
+//	{
+//		static void Main(string[] args)
+//		{
+//			byte[] b = { 1, 2, 3, 4, 5 };
+//			Console.WriteLine("Array is " + BitConverter.ToString(b));
+//			String x = Convert.ToBase64String(b);
+//			Console.WriteLine(x);
+//			Console.ReadLine();
+//		}
+//	}
+//}
