2f7bf809a73d5b1847f736534e93210af1e212c9
labuser <bhoomika>
2023-02-17 09:32:34 +0000

    successfully committed

--- Dotnet2/Dotnet2/Program.cs
+++ Dotnet2/Dotnet2/Program.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.IO;
+﻿//using System;
+//using System.IO;
 
-namespace Dotnet2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string path = "C:\\Users\\labuser\\Desktop\\Student.txt";
-            if (File.Exists(path))
-            {
-                Console.WriteLine("File is present");
-                string[] lines = File.ReadAllLines(path);
-                foreach (var l in lines)
-                {
-                    Console.WriteLine(l);
-                }
-            }
-            else Console.WriteLine("File does not exist");
-            Console.ReadLine();
-        }
-    }
-}
+//namespace Dotnet2
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string path = "C:\\Users\\labuser\\Desktop\\Student.txt";
+//            if (File.Exists(path))
+//            {
+//                Console.WriteLine("File is present");
+//                string[] lines = File.ReadAllLines(path);
+//                foreach (var l in lines)
+//                {
+//                    Console.WriteLine(l);
+//                }
+//            }
+//            else Console.WriteLine("File does not exist");
+//            Console.ReadLine();
+//        }
+//    }
+//}
 
 
 
---
+++ Dotnet2/Dotnet2/looping.cs
@@ -0,0 +1,70 @@
+﻿//using System;
+
+
+//namespace Dotnet2
+//{
+//    class looping
+//    {
+//        static void Main(string[] args)
+//        {
+//            int j,n;
+//            Console.WriteLine("multiplication table");
+//            Console.WriteLine();
+//            n = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("\n");
+//            for(j=1;j<=10;j++)
+//            {
+//                Console.WriteLine(" {0} * {1} ={2} \n", n, j, n * j);
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+
+//using System;
+
+
+//namespace Dotnet2
+//{
+//    class looping
+//    {
+//        static void Main(string[] args)
+//        {
+//            for (int i = 100; i >= 1; i--)
+//            {
+//                Console.WriteLine(i);
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+
+//}
+
+
+using System;
+
+namespace Dotnet2
+{
+    class looping
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            do
+            {
+                Console.WriteLine("enter rollno");
+                int rollno = int.Parse(Console.ReadLine());
+                Console.WriteLine("enter name");
+                String name = Console.ReadLine();
+                Console.WriteLine("Do yo want to continue?");
+                a = Console.ReadLine();
+            }
+            while (a == "yes" || a == "YES" || a == "y" || a == "Y");
+                Console.ReadLine();
+        }
+    }
+}
+
+
+
